9c64543ecc7fc5b6909d1706d94dfe3f4ee1424b
revindaamalias <revindaamalia@gmail.com>
2021-06-02 09:25:12 +0800

    jobsheet12

---
+++ week12/src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCiR3UU6y7MaFj_NtzKFhJZ2MCtl0sP1yM",
+  authDomain: "jobsheet-11---firebase-login.firebaseapp.com",
+  databaseURL:
+    "https://jobsheet-11---firebase-login-default-rtdb.firebaseio.com",
+  projectId: "jobsheet-11---firebase-login",
+  storageBucket: "jobsheet-11---firebase-login.appspot.com",
+  messagingSenderId: "329291912349",
+  appId: "1:329291912349:web:e98d655cfcf74276993074",
+  measurementId: "G-JCLLF3EDZ6",
+};
+export default firebaseConfig;
